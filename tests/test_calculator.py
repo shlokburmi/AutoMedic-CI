f3239022260241179b3d59c94c561feb5e25a9c8
shlokburmi <135860207+shlokburmi@users.noreply.github.com>
2026-04-18 16:34:37 +0530

    Fix module import for CI

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# add project root to path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app.calculator import add
+
 def test_add():
-    assert add(2,3) == 5
+    assert add(2, 3) == 5
